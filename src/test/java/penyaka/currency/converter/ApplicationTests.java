13cef8e3c3355234e7a3c5682647d4f2c0dd2109
VadymPenyaka <112482661+VadymPenyaka@users.noreply.github.com>
2023-09-25 17:35:40 +0300

    add initial project files

---
+++ src/test/java/penyaka/currency/converter/ApplicationTests.java
@@ -0,0 +1,13 @@
+package penyaka.currency.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
